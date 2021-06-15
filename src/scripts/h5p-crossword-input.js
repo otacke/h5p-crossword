f55ca0e83e22de08625fe54b8f12f7505346bb0e
Oliver Tacke <o.tacke@posteo.de>
2021-06-16 01:16:27 +0200

    HFP-3267 Select input field text on click

--- src/scripts/h5p-crossword-input.js
+++ src/scripts/h5p-crossword-input.js
@@ -185,6 +185,8 @@ export default class CrosswordInput {
           return;
         }
 
+        inputField.select();
+
         setTimeout(() => {
           this.callbacks.onFieldInput({
             clueId: word.clueId,
