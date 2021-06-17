2fa17f2914e170af4363c0eff1dfa23a514769ca
Oliver Tacke <o.tacke@posteo.de>
2021-06-17 21:35:28 +0200

    HFP-3266 Use single quotes in entries

--- src/entries/h5p-crossword.js
+++ src/entries/h5p-crossword.js
@@ -1,5 +1,5 @@
-import "../styles/h5p-crossword.scss";
-import Crossword from "../scripts/h5p-crossword";
+import '../styles/h5p-crossword.scss';
+import Crossword from '../scripts/h5p-crossword';
 
 // Load library
 H5P = H5P || {};
