858e2c4546228c91099f83e2110c4d066691eaff
Vilde Voss Stabell <vilde.voss.stabell@gmail.com>
2026-09-22 13:08:45 +0200

    HFP-4305 Make upgrade script work with legacy

--- upgrades.js
+++ upgrades.js
@@ -53,12 +53,14 @@ H5PUpgrades['H5P.Crossword'] = (function () {
           };
 
           if (parameters.behaviour.backgroundImage) {
-            parameters.theme.backgroundImage = parameters.behaviour.backgroundImage;
+            parameters.theme.backgroundImage =
+              parameters.behaviour.backgroundImage;
           }
           delete parameters.behaviour.backgroundImage;
 
           if (parameters.behaviour.backgroundColor) {
-            parameters.theme.backgroundColor = parameters.behaviour.backgroundColor;
+            parameters.theme.backgroundColor =
+              parameters.behaviour.backgroundColor;
           }
           delete parameters.behaviour.backgroundColor;
         }
@@ -66,18 +68,9 @@ H5PUpgrades['H5P.Crossword'] = (function () {
         finished(null, parameters, extras);
       },
       7: function (parameters, finished, extras) {
-        if (parameters?.theme) {
-          delete parameters.theme.gridColor;
-          delete parameters.theme.cellBackgroundColor;
-          delete parameters.theme.cellColor;
-          delete parameters.theme.clueIdColor;
-          delete parameters.theme.cellBackgroundColorHighlight;
-          delete parameters.theme.cellColorHighlight;
-          delete parameters.theme.clueIdColorHighlight;
-
-          if (parameters.theme.backgroundColor === '#173354') {
-            parameters.theme.backgroundColor = 'color-mix(in srgb, var(--h5p-theme-main-cta-base), #000000 50%)';
-          }
+        if (parameters?.theme.backgroundColor === '#173354') {
+          parameters.theme.backgroundColor =
+            'color-mix(in srgb, var(--h5p-theme-main-cta-base, #2e66a8), #000000 50%)';
         }
 
         finished(null, parameters, extras);
